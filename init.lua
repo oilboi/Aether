eaf92d30a0698b2b7b138ed59dba64c6722803de
oilboi <oil.boi.minetest@gmail.com>
2019-02-02 00:15:48 -0500

    fix sharp edges and add in diamondblock decorations

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local minbiomey = 250
 local maxbiomey = 1000
+local seed = 555
 minetest.register_on_generated(function(minp, maxp, blockseed)
   --120 bottom of clouds
 
@@ -7,7 +8,7 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 
     --the () is the line in api doc
     --1 seed (2565) 2 octaves (2580) 3 persistance (2596) scale (2524)
-    local perlin = PerlinNoise(blockseed, 3, 1, 100) --3,10,50 for crazy caves -- 3, 1, 100 for floating islands
+    local perlin = PerlinNoise(seed, 3, 1, 100) --3,10,50 for crazy caves -- 3, 1, 100 for floating islands
 
 
 
@@ -34,13 +35,19 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
 				local p_pos = area:index(x,y,z)
         local p_pos_above = area:index(x,y+1,z)
         local p_pos_below = area:index(x,y-1,z)
-        if perlin:get3d({x=x,y=y,z=z}) > 0.8 then
+        local p_pos_deco = area:index(x,y+2,z)
+        local testerer = perlin:get3d({x=x,y=y,z=z})
+        if testerer > 0.8 then
           --print(tester)
 				  data[p_pos] = stone --this makes the snow adapt to the environment
           data[p_pos_above] =grass -- diamond grass
+          --could use to make grass
+          if testerer >0.85 then
+            data[p_pos_deco] = diamond
+          end
       --  elseif p_pos_below and content_id(p_pos_below) == "aether:heavenly_stone" then
         --    data[p_pos] = grass
-        end
+      end
 		end
 		end
 		end
@@ -52,6 +59,8 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
   end
 
 end)
+
+
 minetest.register_decoration({
 	deco_type = "simple",
 	place_on = "aether:heavenly_stone",
